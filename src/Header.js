a0b68b934670f748437d0d7b1d3ef96b84e20d42
coderDaiyan <daiyanabdallah@gmail.com>
2021-03-26 21:47:00 +0600

    changed some text

--- src/Header.js
+++ src/Header.js
@@ -1,152 +1,137 @@
-import React from 'react'
-import styled from 'styled-components'
-import SearchIcon from '@material-ui/icons/Search';
-import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
-import LocationOnIcon from '@material-ui/icons/LocationOn';
-import {
-    Link
-} from "react-router-dom";
+import LocationOnIcon from "@material-ui/icons/LocationOn";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 function Header() {
-    return (
-        <Container>
-            <HeaderLogo>
-                <Link to="/">
-                    <img src={"https://i.imgur.com/7I9Was5.png"} />
-                </Link>
-            </HeaderLogo>
+  return (
+    <Container>
+      <HeaderLogo>
+        <Link to="/">
+          <img src={"https://i.imgur.com/7I9Was5.png"} />
+        </Link>
+      </HeaderLogo>
 
-            <HeaderOptionAddress>
-                <LocationOnIcon />
-                <HeaderOption>
-                    <OptionLineOne>Hello</OptionLineOne>
-                    <OptionLineTwo>Select Your Address</OptionLineTwo>
-                </HeaderOption>
-            </HeaderOptionAddress>
+      <HeaderOptionAddress>
+        <LocationOnIcon />
+        <HeaderOption>
+          <OptionLineOne>Hello</OptionLineOne>
+          <OptionLineTwo>Select Your Address</OptionLineTwo>
+        </HeaderOption>
+      </HeaderOptionAddress>
 
-            <HeaderSearch>
-                <HeaderSearchInput type='text' />
+      <HeaderSearch>
+        <HeaderSearchInput type="text" />
 
-                <HeaderSearchIconContainer>
-                    <SearchIcon />
-                </HeaderSearchIconContainer>
-            </HeaderSearch>
+        <HeaderSearchIconContainer>
+          <SearchIcon />
+        </HeaderSearchIconContainer>
+      </HeaderSearch>
 
-            <HeaderNavItems>
+      <HeaderNavItems>
+        <HeaderOption>
+          <OptionLineOne>Hello, Daiyan</OptionLineOne>
+          <OptionLineTwo>Account & Lists</OptionLineTwo>
+        </HeaderOption>
 
-                <HeaderOption>
-                    <OptionLineOne>Hello, Nazariy</OptionLineOne>
-                    <OptionLineTwo>Account & Lists</OptionLineTwo>
-                </HeaderOption>
+        <HeaderOption>
+          <OptionLineOne>Returns</OptionLineOne>
+          <OptionLineTwo>& Orders</OptionLineTwo>
+        </HeaderOption>
 
-                <HeaderOption>
-                    <OptionLineOne>Returns</OptionLineOne>
-                    <OptionLineTwo>& Orders</OptionLineTwo>
-                </HeaderOption>
-
-
-                <HeaderOptionCart>
-                    <Link to="/cart">
-                        <ShoppingBasketIcon />
-                        <CartCount>4</CartCount>
-                    </Link>
-                </HeaderOptionCart>
-
-
-
-            </HeaderNavItems>
-
-        </Container>
-    )
+        <HeaderOptionCart>
+          <Link to="/cart">
+            <ShoppingBasketIcon />
+            <CartCount>4</CartCount>
+          </Link>
+        </HeaderOptionCart>
+      </HeaderNavItems>
+    </Container>
+  );
 }
 
-export default Header
+export default Header;
 
 const Container = styled.div`
-    height: 60px;
-    background-color: #0F1111;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    color: white;
-`
+  height: 60px;
+  background-color: #0f1111;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  color: white;
+`;
 
 const HeaderLogo = styled.div`
-    img {
-        width: 100px;
-        margin-left: 11px;
-    }
-`
+  img {
+    width: 100px;
+    margin-left: 11px;
+  }
+`;
 
 const HeaderOptionAddress = styled.div`
-    padding-left: 9px;
-    display: flex;
-    align-items: center;
-    
-`
+  padding-left: 9px;
+  display: flex;
+  align-items: center;
+`;
 
-const OptionLineOne = styled.div`
-
-`
+const OptionLineOne = styled.div``;
 
 const OptionLineTwo = styled.div`
-    font-weight: 700;
-`
+  font-weight: 700;
+`;
 
 const HeaderSearch = styled.div`
-    display: flex;
-    flex-grow: 1;
-    height: 40px;
-    border-radius: 4px;
-    overflow: hidden;
-    margin-left: 4px;
-    background-color: white;
-    :focus-within {
-        box-shadow: 0 0 0 3px #F90;
-    }
-`
-
+  display: flex;
+  flex-grow: 1;
+  height: 40px;
+  border-radius: 4px;
+  overflow: hidden;
+  margin-left: 4px;
+  background-color: white;
+  :focus-within {
+    box-shadow: 0 0 0 3px #f90;
+  }
+`;
 
 const HeaderSearchInput = styled.input`
-    flex-grow: 1;
-    border: 0;
-    :focus {
-        outline: none;
-    }
-`
+  flex-grow: 1;
+  border: 0;
+  :focus {
+    outline: none;
+  }
+`;
 
 const HeaderSearchIconContainer = styled.div`
-    background-color: #febd69;
-    width: 45px;
-    color: black;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    
-`
+  background-color: #febd69;
+  width: 45px;
+  color: black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 const HeaderNavItems = styled.div`
-    display: flex;
-    
-`
+  display: flex;
+`;
 
 const HeaderOption = styled.div`
-    // TRouBLe
-    padding: 10px 9px 10px 9px;
-`
+  // TRouBLe
+  padding: 10px 9px 10px 9px;
+`;
 
 const HeaderOptionCart = styled.div`
+  display: flex;
+  a {
     display: flex;
-    a {
-        display: flex;
-        align-items: center;
-        padding-right: 9px;
-        color: white;
-        text-decoration: none;
-    }
-`
+    align-items: center;
+    padding-right: 9px;
+    color: white;
+    text-decoration: none;
+  }
+`;
 
 const CartCount = styled.div`
-    padding-left: 4px;
-
-`
+  padding-left: 4px;
+`;
--- src/Home.js
+++ src/Home.js
@@ -1,40 +1,38 @@
-import React from 'react'
-import styled from 'styled-components'
-import Product from './Product';
+import React from "react";
+import styled from "styled-components";
+import Product from "./Product";
 
 function Home() {
-    return (
-        <Container>
-            <Banner>
-                
-            </Banner>
-            <Content>
-                <Product />
-                <Product />
-            </Content>
-        </Container>
-    )
+  return (
+    <Container>
+      <Banner></Banner>
+      <Content>
+        <Product />
+        <Product />
+      </Content>
+    </Container>
+  );
 }
 
-export default Home
+export default Home;
 
 const Container = styled.div`
-    max-width: 1500px;
-    margin: 0 auto;
-`
+  max-width: 1500px;
+  margin: 0 auto;
+`;
 
 const Banner = styled.div`
-    background-image: url('https://i.imgur.com/SYHeuYM.jpg');
-    min-height: 600px;
-    background-position: center;
-    background-size: cover;
-    z-index: 1;
-    mask-image: linear-gradient(to bottom, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0));
-`
+  background-image: url("https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_45M_v2_1x._CB432458380_.jpg");
+  min-height: 600px;
+  background-position: center;
+  background-size: cover;
+  z-index: 1;
+  mask-image: linear-gradient(to bottom, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0));
+`;
 
 const Content = styled.div`
-    padding-left: 10px;
-    padding-right: 10px;
-    margin-top: -350px;
-    display: flex;
-`
+  padding-left: 10px;
+  padding-right: 10px;
+  margin-top: -350px;
+  display: flex;
+`;
